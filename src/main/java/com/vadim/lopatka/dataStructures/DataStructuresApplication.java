c0eb7d306b691141327476b2847a01e695ba8aed
Mir <vadim.lopatka@gmail.com>
2018-09-05 11:30:34 +0300

    init

---
+++ src/main/java/com/vadim/lopatka/dataStructures/DataStructuresApplication.java
@@ -0,0 +1,12 @@
+package com.vadim.lopatka.dataStructures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructuresApplication.class, args);
+	}
+}
